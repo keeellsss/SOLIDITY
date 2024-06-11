bf809ea6fef7b8d7fbe42642a41011a0a75b14ba
keeellsss <422003171@ntc.edu.ph>
2024-06-11 20:52:16 +0800

    Update ARGANDA_Token.sol

--- ARGANDA_Token.sol
+++ ARGANDA_Token.sol
@@ -1,25 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
+
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 
 contract ARGANDA_Token {
-    // Public variables to store token details
-    string public name = "MyToken";
-    string public symbol = "MTK";
-    uint256 public totalSupply;
 
-    // Mapping from addresses to balances
-    mapping(address => uint256) public balances;
+    // public variables here
+    string public tokenName = "Cryptokelly";
+    string public tokenAbbrv = "CK";
+    uint public totalSupply = 0;
 
-    // Mint function to increase total supply and balance of the sender
-    function mint(address _to, uint256 _value) public {
+    // mapping variable here
+    mapping (address => uint) public balances;
+    // mint function
+    function mint (address _address, uint _value) public {
         totalSupply += _value;
-        balances[_to] += _value;
+        balances [_address] += _value;    
     }
-
-    // Burn function to decrease total supply and balance of the sender
-    function burn(address _from, uint256 _value) public {
-        require(balances[_from] >= _value, "Balance is not sufficient to burn");
+    // burn function
+    function burn (address _address, uint _value) public {
+        if (balances[_address] >= _value){
         totalSupply -= _value;
-        balances[_from] -= _value;
+        balances [_address] -= _value;  
+        }   
     }
 }
